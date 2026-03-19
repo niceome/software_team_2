fcec9e7bea7834232114bd3043bfcc3434ac30a7
noireed <askqwerty54@gmail.com>
2026-03-19 12:55:35 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI
-from app.api.router import router
-
-app = FastAPI(title="TableMate API")
-
-app.include_router(router)
+fastapi
+uvicorn
+sqlalchemy
+pydantic
+psycopg2-binary
